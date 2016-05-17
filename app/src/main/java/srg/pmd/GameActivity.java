90da6122992371419df70f1cd5ccd19f64414b0b
Sean Wolfe <ether.joe@gmail.com>
2016-05-17 07:09:37 -0700

    now full screen! gotta fix the stretch though.

--- app/src/main/java/srg/pmd/GameActivity.java
+++ app/src/main/java/srg/pmd/GameActivity.java
@@ -1,22 +1,19 @@
 package srg.pmd;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import android.view.Window;
+import android.app.Activity;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_game);
     }
 
