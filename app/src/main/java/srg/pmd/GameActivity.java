32eb67e4ea19dcf2715d864f7c6f1bec811129f2
Sean Wolfe <ether.joe@gmail.com>
2016-06-10 09:35:25 -0700

    spritesheet and background! using layout! better!

--- app/src/main/java/srg/pmd/GameActivity.java
+++ app/src/main/java/srg/pmd/GameActivity.java
@@ -1,8 +1,9 @@
 package srg.pmd;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
-import android.app.Activity;
+import android.view.WindowManager;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -13,8 +14,16 @@ public class GameActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /* create a full screen window */
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
+
+        /*
+        View layout = findViewById(R.id.base_layout);
+        layout.setBackgroundResource(R.drawable.background);
+        */
     }
 
     @Override
