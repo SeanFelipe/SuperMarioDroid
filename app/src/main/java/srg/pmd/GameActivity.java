dcd34441461902489c2c18d37b5010e2270c4bb5
Sean Wolfe <sean@addlightness.tech>
2019-02-11 09:42:15 -0800

    okay have fonts and setup ready. Much faster than expected

--- app/src/main/java/srg/pmd/GameActivity.java
+++ app/src/main/java/srg/pmd/GameActivity.java
@@ -2,12 +2,15 @@ package srg.pmd;
 
 import android.app.Activity;
 import android.graphics.Point;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -18,6 +21,9 @@ public class GameActivity extends Activity {
     RelativeLayout baseLayout;
     ImageView marioSmall;
     ImageView pipe;
+    Typeface marioFont;
+    TextView scoreDesc;
+    TextView score;
     Point screenSize;
     int centerx, centery;
 
@@ -33,10 +39,30 @@ public class GameActivity extends Activity {
         screenSize = new Point();
         getWindowManager().getDefaultDisplay().getSize(screenSize);
 
+        marioFont = Typeface.createFromAsset(getAssets(),  "fonts/mario_font.ttf");
         baseLayout = findViewById(R.id.base_layout);
+
         pipe = findViewById(R.id.pipe);
         pipe.setImageDrawable(getDrawable(R.drawable.pipe));
         float pipeHeight = pipe.getDrawable().getIntrinsicHeight();
+
+        scoreDesc = findViewById(R.id.scoreDesc);
+        scoreDesc.setTypeface(marioFont);
+        scoreDesc.setTextSize(24);
+        scoreDesc.setText("SCORE");
+        scoreDesc.measure(0,0);
+        float scoreDescX = screenSize.x - scoreDesc.getMeasuredWidth() - 100;
+        scoreDesc.setX(scoreDescX);
+        scoreDesc.setY(50);
+
+        score = findViewById(R.id.score);
+        score.setTypeface(marioFont);
+        score.setTextSize(30);
+        score.setText("0");
+        score.measure(0,0);
+        float scoreX = scoreDescX + ( scoreDesc.getMeasuredWidth() / 2 ) - score.getMeasuredWidth() / 2;
+        score.setX(scoreX);
+        score.setY(150);
 
         marioSmall = new ImageView(this);
         marioSmall.setImageDrawable(getDrawable(R.drawable.mario_small));
